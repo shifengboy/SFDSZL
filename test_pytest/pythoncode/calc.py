36ea5ed276771cdaa48b3376081ab3c044b12155
chenshifeng <1162704960@qq.com>
2020-10-16 01:02:14 +0800

    10.15作业提交

---
+++ test_pytest/pythoncode/calc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+'''
+@Auther:陈士锋
+@version: v1.0
+@file: calc.py
+@time: 2020/9/14 9:35 PM
+'''
+# 实现计算器
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
+
